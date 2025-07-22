83a5d0ff3aaeeca95ec4f6ff093775842887a1b4
radek203 <r.porebinski@gmail.com>
2025-07-22 19:08:11 +0200

    Fix dupe with stackable limit enabled

--- src/main/java/com/songoda/skyblock/stackable/Stackable.java
+++ src/main/java/com/songoda/skyblock/stackable/Stackable.java
@@ -24,7 +24,7 @@ public class Stackable {
 
     private Location location;
     private XMaterial material;
-    private int size = 2;
+    private int size = 1;
     private ArmorStand display;
     private int maxSize;
 
